12559ee47f966584706768cbbae7f6f19f884009
Ivan <ulitin.ivan3004@mail.ru>
2024-04-11 12:12:57 +0400

    fix | сделал функцию рекурсивной, убрал preVisitDirectory

--- src/main/java/sgu/ru/B/ArchiveByString.java
+++ src/main/java/sgu/ru/B/ArchiveByString.java
@@ -1,68 +1,48 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.io.*;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ArchiveByString {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         scan.close();
         String delims = "[ ]";
         String[] splitedInput = input.split(delims);
-    
+
         String directory = splitedInput[0];
         String keyString = splitedInput[1];
 
         File dir = new File(directory);
         FileOutputStream fos = new FileOutputStream(directory + ".zip");
         ZipOutputStream zipOut = new ZipOutputStream(fos);
-        for (File curFile : dir.listFiles())
-        {
-            if (curFile.getName().toLowerCase().contains(keyString))
-            {
-                if (curFile.isFile())
-                {
+
+        ArchiveByString archiver = new ArchiveByString();
+        archiver.addToZip(dir, zipOut, keyString, "");
+
+        zipOut.close();
+        fos.close();
+    }
+
+    private void addToZip(File directory, ZipOutputStream zipOut, String keyString, String outPath) throws IOException {
+        for (File curFile : directory.listFiles()) {
+            if (curFile.getName().toLowerCase().contains(keyString) || outPath.contains(keyString)) {
+                if (curFile.isFile()) {
                     FileInputStream fis = new FileInputStream(curFile);
-                    ZipEntry zipEntry = new ZipEntry(curFile.getName());
+                    ZipEntry zipEntry = new ZipEntry(outPath + curFile.getName());
                     zipOut.putNextEntry(zipEntry);
-            
+
                     byte[] bytes = new byte[1024];
                     int length;
-                    while((length = fis.read(bytes)) >= 0) {
+                    while ((length = fis.read(bytes)) >= 0) {
                         zipOut.write(bytes, 0, length);
                     }
                     fis.close();
+                } else {
+                    addToZip(curFile, zipOut, keyString, outPath + "/" + curFile.getName() + "/");
                 }
-                else
-                {
-                    Files.walkFileTree(curFile.toPath(), new SimpleFileVisitor<Path>() {
-                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                            String newZipFileName = dir.toPath().relativize(file).toString();
-                            zipOut.putNextEntry(new ZipEntry(newZipFileName));
-                            Files.copy(file, zipOut);
-                            zipOut.closeEntry();
-                            return FileVisitResult.CONTINUE;
-                        }
-
-                        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                            zipOut.putNextEntry(new ZipEntry(curFile.toPath().relativize(dir).toString() + "/"));
-                            zipOut.closeEntry();
-                            return FileVisitResult.CONTINUE;
-                        }
-                    });
-                }
-            };
+            }
         }
-        zipOut.close();
-        fos.close();
     }
 }
